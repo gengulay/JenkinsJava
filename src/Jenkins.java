11b74a8b33d623266e88d5d193108706c67f07f5
Gen Gulay <gengulay@gmail.com>
2019-03-22 18:28:46 +0800

    modified message

--- src/Jenkins.java
+++ src/Jenkins.java
@@ -2,7 +2,7 @@
 public class Jenkins {
 
 	public static void main(String[] args) {
-		System.out.println("Test JOB!");
+		System.out.println("Test JOB!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
 	}
 
 }
